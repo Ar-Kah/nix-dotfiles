f045986970c5a57d6e7e88a1fd3aec181372d213
Aaro Karhu <aaro.karhu19@gmail.com>
2026-09-15 11:11:58 +0800

    added a comment

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -126,6 +126,7 @@ local terminal     = "alacritty"
 local vi_focus     = false -- vi-like client focus https://github.com/lcpz/awesome-copycats/issues/275
 local cycle_prev   = true  -- cycle with only the previously focused client or all https://github.com/lcpz/awesome-copycats/issues/274
 local editor       = os.getenv("EDITOR") or "nvim"
+-- default brouser
 local browser      = "librewolf"
 
 awful.util.terminal = terminal
