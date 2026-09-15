908fddeda7b6a3a34b5c530cb0eb230d70cf7140
Aaro Karhu <aaro.karhu19@gmail.com>
2026-09-15 11:45:58 +0800

    change default brouser

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -127,7 +127,7 @@ local vi_focus     = false -- vi-like client focus https://github.com/lcpz/aweso
 local cycle_prev   = true  -- cycle with only the previously focused client or all https://github.com/lcpz/awesome-copycats/issues/274
 local editor       = os.getenv("EDITOR") or "nvim"
 -- default brouser
-local browser      = "librewolf"
+local browser      = "google-chrome-stable"
 
 awful.util.terminal = terminal
 awful.util.tagnames = { "1", "2", "3", "4", "5" }
